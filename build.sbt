63f4c5a71da50f56a836216120780c6674cf91e7
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-06-23 21:06:47 -0700

    First commit

---
+++ build.sbt
@@ -0,0 +1,29 @@
+import sbt._
+
+name := "mahout_samples"
+
+organization := "com.ldaniels528"
+
+version := "0.1"
+
+scalaVersion := "2.11.6"
+
+scalacOptions ++= Seq("-deprecation", "-encoding", "UTF-8", "-feature", "-target:jvm-1.7", "-unchecked",
+  "-Ywarn-adapted-args", "-Ywarn-value-discard", "-Xlint")
+
+javacOptions ++= Seq("-Xlint:deprecation", "-Xlint:unchecked", "-source", "1.7", "-target", "1.7", "-g:vars")
+
+// General Dependencies
+libraryDependencies ++= Seq(
+  "log4j" % "log4j" % "1.2.17",
+  "org.apache.mahout" % "mahout-core" % "0.9",
+  "org.slf4j" % "slf4j-api" % "1.7.7",
+  "org.slf4j" % "slf4j-log4j12" % "1.7.7"
+)
+
+// Testing Dependencies
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.11" % "test",
+  "org.mockito" % "mockito-all" % "1.9.5" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.2" % "test"
+)
---
+++ src/main/scala/org/ldaniels528/mahout/SampleRecommendation.scala
@@ -0,0 +1,58 @@
+package org.ldaniels528.mahout
+
+import java.io.File
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity
+import org.slf4j.LoggerFactory
+import scala.collection.JavaConversions._
+
+/**
+ * Sample Recommendation
+ * @author lawrence.daniels@gmail.com
+ */
+object SampleRecommendation {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  def main(args: Array[String]): Unit = {
+    /*
+     * The first thing we have to do is load the data from the file.
+     * Mahout's recommenders use an interface called DataModel to handle interaction data.
+     * You can load our made up interactions like this:
+     */
+    val model = new FileDataModel(new File("./src/main/resources/dataset.csv"))
+
+    /*
+     * In this example, we want to create a user-based recommender. The idea behind this approach is that
+     * when we want to compute recommendations for a particular users, we look for other users with a similar
+     * taste and pick the recommendations from their items. For finding similar users, we have to compare
+     * their interactions. There are several methods for doing this. One popular method is to compute
+     * the correlation coefficient between their interactions. In Mahout, you use this method as follows:
+     */
+    val similarity = new PearsonCorrelationSimilarity(model)
+
+    /*
+     * he next thing we have to do is to define which similar users we want to leverage for the recommender.
+     * For the sake of simplicity, we'll use all that have a similarity greater than 0.1. This is implemented
+     * via a ThresholdUserNeighborhood:
+     */
+    val neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model)
+
+    /*
+     * Now we have all the pieces to create our recommender:
+     */
+    val recommender = new GenericUserBasedRecommender(model, neighborhood, similarity)
+
+    /*
+     * We can easily ask the recommender for recommendations now. If we wanted to get
+     * three items recommended for the user with userID 2, we would do it like this:
+     */
+    val recommendations = recommender.recommend(2, 3)
+    for (recommendation <- recommendations) {
+      logger.info(s"recommendation: $recommendation")
+    }
+  }
+
+}
