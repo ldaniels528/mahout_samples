24deea47984d9285a1e2483de1c14fd2f4e02e14
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-06-23 21:15:09 -0700

    updated the documentation

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# mahout_samples
-Experimenting with Apache Mahout using Scala
+# Apache Mahout Samples
+
+This project is a small collection of experiments with Apache Mahout using Scala. My first experiment is a 
+quick-and-dirty user recommendation engine using a small data set (a file containing 32 entries).
+
+Data Set:
+```
+1,10,1.0
+1,11,2.0
+1,12,5.0
+1,13,5.0
+1,14,5.0
+1,15,4.0
+1,16,5.0
+1,17,1.0
+1,18,5.0
+2,10,1.0
+2,11,2.0
+2,15,5.0
+2,16,4.5
+2,17,1.0
+2,18,5.0
+3,11,2.5
+3,12,4.5
+3,13,4.0
+3,14,3.0
+3,15,3.5
+3,16,4.5
+3,17,4.0
+3,18,5.0
+4,10,5.0
+4,11,5.0
+4,12,5.0
+4,13,0.0
+4,14,2.0
+4,15,3.0
+4,16,1.0
+4,17,4.0
+4,18,1.0
+```
+
+Expected output:
+```
+2015-06-23 21:13:49 INFO  FileDataModel:187 - Creating FileDataModel for file ./src/main/resources/dataset.csv
+2015-06-23 21:13:50 INFO  FileDataModel:358 - Reading file info...
+2015-06-23 21:13:50 INFO  FileDataModel:369 - Read lines: 32
+2015-06-23 21:13:50 INFO  GenericDataModel:113 - Processed 4 users
+2015-06-23 21:13:50 INFO  SampleRecommendation$:59 - recommendation: RecommendedItem[item:12, value:4.8328104]
+2015-06-23 21:13:50 INFO  SampleRecommendation$:59 - recommendation: RecommendedItem[item:13, value:4.6656213]
+2015-06-23 21:13:50 INFO  SampleRecommendation$:59 - recommendation: RecommendedItem[item:14, value:4.331242]
+```
--- src/main/scala/org/ldaniels528/mahout/SampleRecommendation.scala
+++ src/main/scala/org/ldaniels528/mahout/SampleRecommendation.scala
@@ -7,6 +7,7 @@ import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity
 import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConversions._
 
 /**
@@ -16,7 +17,12 @@ import scala.collection.JavaConversions._
 object SampleRecommendation {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def main(args: Array[String]): Unit = {
+  /**
+   * Main application entry point
+   * @param args the given command line arguments
+   */
+  def main(args: Array[String]) {
+
     /*
      * The first thing we have to do is load the data from the file.
      * Mahout's recommenders use an interface called DataModel to handle interaction data.
@@ -25,7 +31,7 @@ object SampleRecommendation {
     val model = new FileDataModel(new File("./src/main/resources/dataset.csv"))
 
     /*
-     * In this example, we want to create a user-based recommender. The idea behind this approach is that
+     * In this example, we want to create a user-based recommendation engine. The idea behind this approach is that
      * when we want to compute recommendations for a particular users, we look for other users with a similar
      * taste and pick the recommendations from their items. For finding similar users, we have to compare
      * their interactions. There are several methods for doing this. One popular method is to compute
@@ -34,23 +40,22 @@ object SampleRecommendation {
     val similarity = new PearsonCorrelationSimilarity(model)
 
     /*
-     * he next thing we have to do is to define which similar users we want to leverage for the recommender.
+     * The next thing we have to do is to define which similar users we want to leverage for the recommendation engine.
      * For the sake of simplicity, we'll use all that have a similarity greater than 0.1. This is implemented
      * via a ThresholdUserNeighborhood:
      */
     val neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model)
 
     /*
-     * Now we have all the pieces to create our recommender:
+     * Now we have all the pieces to create our recommendation engine:
      */
     val recommender = new GenericUserBasedRecommender(model, neighborhood, similarity)
 
     /*
-     * We can easily ask the recommender for recommendations now. If we wanted to get
+     * We can easily ask the recommendation engine for recommendations now. If we wanted to get
      * three items recommended for the user with userID 2, we would do it like this:
      */
-    val recommendations = recommender.recommend(2, 3)
-    for (recommendation <- recommendations) {
+    for (recommendation <- recommender.recommend(2, 3)) {
       logger.info(s"recommendation: $recommendation")
     }
   }
